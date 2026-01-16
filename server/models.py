cb77e60789166eb0060b75ef15c56c3e6b76c76c
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-16 15:30:05 +0300

    added to app.py

--- server/models.py
+++ server/models.py
@@ -1,9 +1,9 @@
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
+class Plant(db.Model):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -12,5 +12,11 @@ class Plant(db.Model, SerializerMixin):
     price = db.Column(db.Float)
     is_in_stock = db.Column(db.Boolean)
 
-    def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+    def to_dict(self):
+      return {
+        "id": self.id,
+        "name": self.name,
+        "image": self.image,
+        "price": self.price,
+        "is_in_stock": self.is_in_stock
+    }
